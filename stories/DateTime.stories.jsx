087d7b92b01d9863db63dc82e002f7a0fc660c25
Zainab Ebrahimi <zainabebrahimi@gmail.com>
2021-02-04 11:08:48 -0500

    add storybook

---
+++ stories/DateTime.stories.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import DateTime from '../components/shared/dateTime';
+
+export default {
+  title: 'DateTime',
+  component: DateTime
+};
+
+export const Default = (args) => <DateTime overline='A great question' title='This is a question' buttonText='Next' name='defaultDateTime'/>;
+
---
+++ stories/Statement.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Statement from '../components/shared/statement';
+
+export default {
+  title: 'Statement',
+  component: Statement
+};
+
+export const Default = (args) => <Statement overline='A great question' title='This is a question' buttonText='Next'/>;
+
