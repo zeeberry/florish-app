7c3b5b010ead0aa6f85ad33377687890a3941c51
Zainab Ebrahimi <zainabebrahimi@gmail.com>
2021-03-04 15:48:42 -0500

    init fauna db + graphql (#1)

---
+++ graphql/api.js
@@ -0,0 +1,112 @@
+import useFetch from '../util/useFetch'
+
+function getData(data) {
+  if (!data || data.errors) return null
+  return data.data
+}
+
+function getErrorMessage(error, data) {
+  if (error) return error.message
+  if (data && data.errors) {
+    return data.errors[0].message
+  }
+  return null
+}
+
+export const getProfileByEmail = (email) => {
+  const query = `query AccountByEmail($email: String!) {
+    accountByEmail(email: $email) {
+      data {
+        _id
+        _ts
+        email
+        profile {
+          name
+          applications {
+            data {
+              company
+              role
+              interviews {
+                data {
+                  type
+                }  
+              }
+            }
+          }
+        }
+      }
+      after
+    }
+  }`
+  const { data, error } = useFetch(
+    process.env.NEXT_PUBLIC_FAUNADB_GRAPHQL_ENDPOINT,
+    {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_FAUNADB_SECRET}`,
+        'Content-type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify({
+        query,
+        variables: { email },
+      }),
+    }
+  )
+
+  return {
+    data: getData(data),
+    errorMessage: getErrorMessage(error, data),
+    error,
+  }
+}
+
+export const createAccount = async (email, name, company, role, date, type, notes) => {
+  const query = `mutation CreateAccount($email: String!, $name: String!, $company: String!, $role: String!, $date: String!, $type: String!, $notes: String!) {
+    createAccount(data: {
+      email: $email
+      profile: {
+        create: { 
+          name: $name
+          applications: {
+            create: {
+              company: $company  
+              role: $role
+              initialExcitement: 1
+              currentExcitment: 3 
+              interviews: {
+                create: {
+                  date: $date 
+                  type: $type
+                  nerves: 4 
+                  notes: $notes
+                  excitement: 2
+                }
+              }
+            }
+          }
+        }
+      }
+    }) {
+      _id
+      _ts
+      email
+    }
+  }`
+
+  const res = await fetch(process.env.NEXT_PUBLIC_FAUNADB_GRAPHQL_ENDPOINT, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_FAUNADB_SECRET}`,
+      'Content-type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify({
+      query,
+      variables: { email, name, company, role, date, type, notes},
+    }),
+  })
+  const data = await res.json()
+
+  return data
+};
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,12 +1,40 @@
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { useContext } from 'react';
+import { getProfileByEmail } from '../graphql/api';
 import Context from '../store/context';
 
+const getProfile = (data) => {
+  return data ? data.accountByEmail.data : [];
+}
+
 export default function Dashboard() {
+  const { data, errorMessage } = getProfileByEmail('zainab@florish.tech');
+  const [ profile, setProfile ] = useState([]);
   const { state } = useContext(Context);
+
+  useEffect(() => {
+    if (!profile.length) {
+      setProfile(getProfile(data))
+    }
+  }, [data, profile.length]);
+
   return (
     <>
-      {state.user.name}
+      <section>
+      {!data ? (
+          <p>Loading entries...</p>
+      ) : profile.map((entry, index, allEntries) => {
+          const date = new Date(entry._ts / 1000);
+          return (
+            <p key={entry._id}>
+              {entry.email}
+            </p>
+          )
+        })
+      }
+      <div>{state.user.name}</div>
+      </section>
     </>
   );
 };
