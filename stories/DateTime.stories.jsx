2d1ff2db02543f6ab9819208694ede79988d9213
Zainab Ebrahimi <zainabebrahimi@gmail.com>
2021-03-05 15:24:13 -0500

    clean up stories

--- stories/DateTime.stories.jsx
+++ stories/DateTime.stories.jsx
@@ -3,9 +3,17 @@ import React from 'react';
 import DateTime from '../components/shared/dateTime';
 
 export default {
-  title: 'DateTime',
+  title: 'Form/DateTime',
   component: DateTime
 };
 
-export const Default = (args) => <DateTime overline='A great question' title='This is a question' buttonText='Next' name='defaultDateTime'/>;
+const Template = (args) => <DateTime {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  overline: 'A great question',
+  title: 'This is a question',
+  buttonText: 'Next',
+  name: 'defaultDateTime'
+};
 
