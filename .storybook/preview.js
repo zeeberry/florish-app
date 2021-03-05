42f50ae9d7382d508f5c687ca4e5d03feea86b8d
Zainab Ebrahimi <zainabebrahimi@gmail.com>
2021-03-05 15:25:16 -0500

    import globalStyles for storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,22 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import theme from '../theme/theme';
+import GlobalStyle from '../theme/globalStyle';
+import { Main, Content } from '../components/shared/elements';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-}
+};
+
+export const decorators = [
+  (Story) => (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Content>
+          <Story />
+        </Content>
+      </ThemeProvider>
+    </>
+  ),
+];
